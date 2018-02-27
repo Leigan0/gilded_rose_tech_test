4930685baab83b16888799ea8c23bc7af51e3db9
Leigh-ann <leighann.gant@gmail.com>
2018-02-27 10:47:08 +0000

    Tests added for sell_in date to rspec file

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,33 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  let(:items) { [Item.new('foo', 5, 5)] }
+  let(:gilded_rose) { described_class.new(items) }
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+  describe '#update_quality' do
+    it 'does not change the name' do
+      gilded_rose.update_quality
+      expect(items[0].name).to eq 'foo'
+    end
+    context 'items sell_in date' do
+      context 'sell in date for Sulfuras items' do
+        it 'does not change sell in date for Sulfuras items' do
+          items = [Item.new('Sulfuras, Hand of Ragnaros', 5, 5)]
+          gilded_rose = described_class.new(items)
+          gilded_rose.update_quality
+          expect(items[0].sell_in).to eq 5
+        end
+      end
+      context 'all other items sell_in date' do
+        it 'reduces an item sell_in attribute by one' do
+          gilded_rose.update_quality
+          expect(items[0].sell_in).to eq 4
+        end
+        it 'allows an item sell_in date to go below zero' do
+          6.times { gilded_rose.update_quality }
+          expect(items[0].sell_in).to eq -1
+        end
+      end
     end
   end
-
 end
--- spec/texttest_fixture.rb
+++ spec/texttest_fixture.rb
@@ -5,8 +5,8 @@ items = [
   Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
   Item.new(name="Aged Brie", sell_in=2, quality=0),
   Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=50),
+  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=50),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
@@ -29,3 +29,87 @@ gilded_rose = GildedRose.new items
   puts ""
   gilded_rose.update_quality
 end
+
+# OMGHAI!
+# -------- day 0 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 10, 20
+# Aged Brie, 2, 0
+# Elixir of the Mongoose, 5, 7
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 15, 20
+# Backstage passes to a TAFKAL80ETC concert, 10, 49
+# Backstage passes to a TAFKAL80ETC concert, 5, 49
+
+
+# -------- day 1 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 9, 19
+# Aged Brie, 1, 1
+# Elixir of the Mongoose, 4, 6
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 14, 21
+# Backstage passes to a TAFKAL80ETC concert, 9, 50
+# Backstage passes to a TAFKAL80ETC concert, 4, 50
+#
+#
+# -------- day 2 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 8, 18
+# Aged Brie, 0, 2
+# Elixir of the Mongoose, 3, 5
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 13, 22
+# Backstage passes to a TAFKAL80ETC concert, 8, 50
+# Backstage passes to a TAFKAL80ETC concert, 3, 50
+
+
+# -------- day 3 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 7, 17
+# Aged Brie, -1, 4
+# Elixir of the Mongoose, 2, 4
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 12, 23
+# Backstage passes to a TAFKAL80ETC concert, 7, 50
+# Backstage passes to a TAFKAL80ETC concert, 2, 50
+# Conjured Mana Cake, 0, 3
+#
+# -------- day 4 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 6, 16
+# Aged Brie, -2, 6
+# Elixir of the Mongoose, 1, 3
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 11, 24
+# Backstage passes to a TAFKAL80ETC concert, 6, 50
+# Backstage passes to a TAFKAL80ETC concert, 1, 50
+
+
+# -------- day 5 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 5, 15
+# Aged Brie, -3, 8
+# Elixir of the Mongoose, 0, 2
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 10, 25
+# Backstage passes to a TAFKAL80ETC concert, 5, 50
+# Backstage passes to a TAFKAL80ETC concert, 0, 50
+
+
+# -------- day 6 --------
+# name, sellIn, quality
+# +5 Dexterity Vest, 4, 14
+# Aged Brie, -4, 10
+# Elixir of the Mongoose, -1, 0
+# Sulfuras, Hand of Ragnaros, 0, 50
+# Sulfuras, Hand of Ragnaros, -1, 50
+# Backstage passes to a TAFKAL80ETC concert, 9, 27
+# Backstage passes to a TAFKAL80ETC concert, 4, 50
+# Backstage passes to a TAFKAL80ETC concert, -1, 0
