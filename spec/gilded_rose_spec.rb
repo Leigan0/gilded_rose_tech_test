b7822cbb33b1a72373b40fb82e75e872a18f7352
Leigh-ann <leighann.gant@gmail.com>
2018-02-27 12:52:04 +0000

    tests refactored to DRY up code, tests added for sulfuras quality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,32 +1,34 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:brie) { [Item.new('Aged Brie', 1, 46)] }
-  let(:gilded_rose_brie) { described_class.new(brie) }
-  let(:items) { [Item.new('foo', 5, 0)] }
+  let(:backstagepasses) { [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 1)] }
+  let(:gilded_rose_pass) { described_class.new(backstagepasses)}
+
+  let(:bries) { [Item.new('Aged Brie', 1, 46), Item.new('Aged Brie', -1, 46), Item.new('Aged Brie', -1, 50)] }
+  let(:gilded_rose_brie) { described_class.new(bries) }
+
+  let(:items) { [Item.new('foo', 1, 0), Item.new('Sulfuras, Hand of Ragnaros', 5, 5)] }
   let(:gilded_rose) { described_class.new(items) }
 
   describe '#update_quality' do
-    it 'does not change the name' do
+    before(:each) do
       gilded_rose.update_quality
+    end
+    it 'does not change the name' do
       expect(items[0].name).to eq 'foo'
     end
     describe 'items sell_in date' do
       context 'sell in date for Sulfuras items' do
         it 'does not change sell in date for Sulfuras items' do
-          items = [Item.new('Sulfuras, Hand of Ragnaros', 5, 5)]
-          gilded_rose = described_class.new(items)
-          gilded_rose.update_quality
-          expect(items[0].sell_in).to eq 5
+          expect(items[1].sell_in).to eq 5
         end
       end
       context 'all other items sell_in date' do
         it 'reduces an item sell_in attribute by one' do
-          gilded_rose.update_quality
-          expect(items[0].sell_in).to eq 4
+          expect(items[0].sell_in).to eq 0
         end
         it 'allows an item sell_in date to go below zero' do
-          6.times { gilded_rose.update_quality }
+          gilded_rose.update_quality
           expect(items[0].sell_in).to eq -1
         end
       end
@@ -34,21 +36,27 @@ describe GildedRose do
 
     describe 'item quality attribute' do
       it 'will not drop item quality be below 0' do
-        gilded_rose.update_quality
         expect(items[0].quality).to eq 0
       end
+
+      context 'sell in date for Sulfuras items' do
+        it 'will not alter the quality attribute for Sulfuras items' do
+          expect(items[1].quality).to eq 5
+        end
+      end
+
       context 'Aged Brie items' do
-        it 'increases in quality by one for each remaining sell by date' do
+        before(:each) do
           gilded_rose_brie.update_quality
-          expect(brie[0].quality).to eq 47
+        end
+        it 'increases in quality by one for each remaining sell by date' do
+          expect(bries[0].quality).to eq 47
         end
         it 'increases in quality twice as fast when Aged Brie sell_in < 0' do
-          2.times { gilded_rose_brie.update_quality }
-          expect(brie[0].quality).to eq 49
+          expect(bries[1].quality).to eq 48
         end
         it 'will not raise item quality above 50 theshold' do
-          3.times { gilded_rose_brie.update_quality }
-          expect(brie[0].quality).to eq 50
+          expect(bries[2].quality).to eq 50
         end
       end
     end
